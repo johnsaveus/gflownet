7148ffd3f44a3d92d53d8acac97fa70ba1ccdbf5
john savvas <ioannis.savvas00@gmail.com>
2025-03-24 13:48:17 +0200

    training_pipeline

--- src/gflownet/proxy/main.py
+++ src/gflownet/proxy/main.py
@@ -1,10 +1,14 @@
+import torch
+import random
 import pandas as pd
+import numpy as np
 from rdkit import Chem
 from gflownet.proxy.model import GraphAttention
 from gflownet.proxy.mol_utils import scaffold_split, smiles2graph
-from gflownet.proxy.train_utils import build_loaders
+from gflownet.proxy.train_utils import build_loaders, train_epoch, eval_epoch, infer_model, get_metrics, plot_results
 
 if __name__ == "__main__":
+    # ------------  Hyperparameters for tuning
     hparams = {
         "learning_rate": [0.0001, 0.0005, 0.001],
         "batch_size": [64, 128, 256],
@@ -15,6 +19,14 @@ if __name__ == "__main__":
         "mlp_channels": [32, 64, 128],
         "dropout_proba": [0.1, 0.2, 0.3],
     }
+    # ------------  Set seed
+    seed = 42
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
     # ------------  Load data
     data_url = r"https://raw.githubusercontent.com/CesareWang/Predictors-for-15-Environmental-Endpoints/main/predictors/data/KOW.csv"
     kow_data = pd.read_csv(data_url, index_col=0).iloc[:300]
@@ -39,20 +51,37 @@ if __name__ == "__main__":
         smiles2graph(Chem.MolFromSmiles(smiles), y) for smiles, y in zip(test_data["smiles"], test_data["logKOW"])
     ]
     # ------------ Loaders
-    tr_loader, val_loader, te_loader = build_loaders(train_dataset, valid_dataset, test_dataset, batch_size=64)
+    tr_loader, val_loader, te_loader = build_loaders(train_dataset, valid_dataset, test_dataset, batch_size=128)
 
     # ------------- Build Model
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = GraphAttention(
         node_feats=29,
         edge_dim=7,
         gnn_layers=2,
-        gnn_channels=16,
-        heads=2,
-        dropout_proba=0.1,
+        gnn_channels=32,
+        heads=4,
+        dropout_proba=0.3,
         gnn_norm=True,
         mlp_layers=2,
-        mlp_channels=16,
+        mlp_channels=32,
         mlp_norm=True,
-    )
+    ).to(device)
+
+    # --------------- Training configs
+    epochs = 20
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.1, verbose=True)
+
     # --------------- Training
+    for epoch in range(epochs):
+        train_loss = train_epoch(model, tr_loader, optimizer, scheduler, device)
+        val_loss = eval_epoch(model, val_loader, device)
+        print(f"Epoch: {epoch}, Train Loss: {train_loss}, Val Loss: {val_loss}")
+
+    # Inference
+    all_preds, all_true = infer_model(model, te_loader, device)
+    rmse, mae, r2 = get_metrics(all_preds, all_true)
+    print(f"RMSE: {rmse}, MAE: {mae}, R2: {r2}")
+
+    plot_results(all_preds, all_true)
--- src/gflownet/proxy/train_utils.py
+++ src/gflownet/proxy/train_utils.py
@@ -1,8 +1,83 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from torch_geometric.loader import DataLoader
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error, r2_score
 
 
 def build_loaders(train_dataset, valid_dataset, test_dataset, batch_size=64):
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
     return train_loader, valid_loader, test_loader
+
+
+def train_epoch(model, train_loader, optimizer, scheduler, device):
+    model.train()
+    total_loss = 0
+    for data in train_loader:
+        optimizer.zero_grad()
+        x, edge_index, edge_attr, batch, y = data_from_loader(data, device)
+        pred = model(x, edge_index, edge_attr, batch)
+        loss = F.mse_loss(pred.squeeze(dim=-1), y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    total_loss /= len(train_loader)
+    scheduler.step(total_loss)
+    return total_loss
+
+
+def eval_epoch(model, val_loader, device):
+    model.eval()
+    total_loss = 0
+    for data in val_loader:
+        x, edge_index, edge_attr, batch, y = data_from_loader(data, device)
+        with torch.no_grad():
+            pred = model(x, edge_index, edge_attr, batch)
+        loss = F.mse_loss(pred.squeeze(dim=-1), y)
+        total_loss += loss.item()
+    return total_loss / len(val_loader)
+
+
+def infer_model(model, loader, device):
+    all_preds = []
+    all_true = []
+    for data in loader:
+        x, edge_index, edge_attr, batch, y = data_from_loader(data, device)
+        with torch.no_grad():
+            pred = model(x, edge_index, edge_attr, batch)
+        pred_list = [pr for pr in pred.squeeze(dim=-1).cpu().numpy()]
+        all_preds.extend(pred_list)
+        true_list = [tr for tr in y.cpu().numpy()]
+        all_true.extend(true_list)
+    return all_preds, all_true
+
+
+def get_metrics(preds, true):
+    rmse = root_mean_squared_error(true, preds)
+    mae = mean_absolute_error(true, preds)
+    r2 = r2_score(true, preds)
+
+    return rmse, mae, r2
+
+
+def plot_results(preds, true):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(true, preds, alpha=0.5, edgecolors="w", marker="o", linewidth=0.2)
+    plt.plot([min(true), max(true)], [min(true), max(true)], color="red", linestyle="--", linewidth=0.5)
+    plt.xlabel("LogP", fontsize=14)
+    plt.ylabel("Predicted LogP", fontsize=14)
+    # plt.title("True vs Predicted Values", fontsize=16)
+    # plt.grid(True)
+    plt.savefig("results.png", bbox_inches="tight")
+
+
+def data_from_loader(data, device):
+    x = data.x.to(device)
+    edge_index = data.edge_index.to(device)
+    edge_attr = data.edge_attr.to(device)
+    batch = data.batch.to(device)
+    y = data.y.to(device)
+
+    return x, edge_index, edge_attr, batch, y
